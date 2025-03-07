6081aa7905bd6681a4e063db1544c7a92586ec2b
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2025-03-07 11:29:58 +0530

    resolver function with relatios objects

--- graphql-js/src/index.js
+++ graphql-js/src/index.js
@@ -2,56 +2,62 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+
 //db
 import _db from "./_db.js";
-
 //type
-import { typeDefs } from "./schema.js"
+import { typeDefs } from "./schema.js";
 
 
 const resolvers={
-   Query:{ 
-    games(){
-       return _db.games
-    }
-    ,
-    game(_,args){
 
-        return _db.games.find((game)=> String(game.id)===args.id)
-    },
-    authors(){
-        return _db.authors
-    },
-    author(_,args){
+    Query: {
+        games(){
+            return _db.games
+        },
+        game(_,args){
 
-        return _db.authors.find((author)=> String(author.id)===args.id)
-    },
-    reviews(){
-        return _db.reviews
-    },
-    review(_,args){
+            return _db.games.find(g=> g.id===args.id)
 
-        return _db.reviews.find((review)=> String(review.id)===args.id)
-    }
+        },
+        reviews(){
+            return _db.reviews
+        },
+        review(_,args){
+
+            return _db.reviews.find(r=> r.id===args.id)
+
+        },
+        authors(){
+            return _db.authors
+        },
+        author(_,args){
+
+            return _db.authors.find(a=> a.id===args.id)
+
+        }
     },
     Game:{
+
         reviews(parent){
-            return _db.reviews.filter(r=>r.gameId ===parent.id)
+            return _db.reviews.filter(r=> r.gameId===parent.id)
         }
     },
     Author:{
         reviews(parent){
-            return _db.reviews.filter(r=>r.authorId ===parent.id)
+            return _db.reviews.filter(a=> a.authorId===parent.id)
         }
+
     },
     Review:{
-        author(parent){
-        return _db.authors.filter(a=>String(a.id) ===parent.authorId)
-        },
         game(parent){
-            return _db.games.filter(g=>String(g.id) ===parent.gameId)
-            }
+            return _db.games.find(g=>g.id===parent.gameId)
+        },
+        author(parent){
+            return _db.authors.find(a=> a.id===parent.authorId)
+        }
     }
+
 }
 
 //setup server
--- graphql-js/src/schema.js
+++ graphql-js/src/schema.js
@@ -1,35 +1,95 @@
 
 export const typeDefs=`#graphql
 
-type Game {
-    id: ID!
-    title: String!
-    platform: [String!]!
+type Game{
+    id:ID!
+    title:String!
+    platform:[String!]!
     reviews:[Review!]
+}
 
-    }
 type Review {
-    id: ID!
-    rating: Int!
-    content: String!
+    id:ID!
+    rating:Int!
+    content:String!
     game:Game!
     author:Author!
-    }
-
-type Author {
-    id: ID!
-    name: String!
+}
+type Author{
+    id:ID!
+    name:String!
     verified: Boolean!
     reviews:[Review!]
-    }
-    type Query{
-    reviews: [Review]
-    review(id: ID!):Review
-    games: [Game]
-    game(id:ID!): Game
-    authors: [Author]
-    author(id:ID!): Author
-    }
-
+}
+type Query{
+    games:[Game]
+    game(id:ID!):Game
+    reviews:[Review]
+    review(id:ID!):Review
+    authors:[Author]
+    author(id:ID!):Author
+}
 
 `
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const typeDefs=`#graphql
+
+// type Game {
+//     id: ID!
+//     title: String!
+//     platform: [String!]!
+//     reviews:[Review!]
+
+//     }
+// type Review {
+//     id: ID!
+//     rating: Int!
+//     content: String!
+//     game:Game!
+//     author:Author!
+//     }
+
+// type Author {
+//     id: ID!
+//     name: String!
+//     verified: Boolean!
+//     reviews:[Review!]
+//     }
+//     type Query{
+//     reviews: [Review]
+//     review(id: ID!):Review
+//     games: [Game]
+//     game(id:ID!): Game
+//     authors: [Author]
+//     author(id:ID!): Author
+//     }
+
+// `
